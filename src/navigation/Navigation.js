60c2d741e300b1151765ac17dbfeb65205de9a78
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-19 12:54:35 -0300

    init project and add screens and navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Account from "../screens/Account";
+import Favorite from "../screens/Favorite";
+import Pokedex from "../screens/Pokedex";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { Image } from "react-native";
+import PokedexNavigation from "./PokedexNavigation";
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          headerTitle: "Favoritos",
+          tabBarLabel: "Favoritos",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexNavigation}
+        options={{
+          tabBarLabel: "",
+          headerTitleAlign: "center",
+          tabBarIcon: () => renderPokeBall(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          headerTitle: "Mi cuenta",
+          tabBarLabel: "Mi cuenta",
+          headerTitleAlign: "center",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+function renderPokeBall() {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{
+        width: 75,
+        height: 75,
+        top: -15,
+      }}
+    />
+  );
+}
+
+export default Navigation;
---
+++ src/screens/Favorite.js
@@ -0,0 +1,10 @@
+import { Text, SafeAreaView } from "react-native";
+import React from "react";
+
+export default function Favorite() {
+  return (
+    <SafeAreaView>
+      <Text>Favorite</Text>
+    </SafeAreaView>
+  );
+}
