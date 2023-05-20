e9f0a6fd28e9b2fee591005cd4836edec49bcd70
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-20 20:16:21 -0300

    feat: implemented formik and yup

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 import {
   View,
   Text,
@@ -7,8 +7,19 @@ import {
   Button,
   Keyboard,
 } from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("enviado");
+    },
+  });
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
@@ -16,17 +27,37 @@ export default function LoginForm() {
         placeholder="Nombre de usuario"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(username) => formik.setFieldValue("username", username)}
       />
       <TextInput
         placeholder="Contraseña"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(password) => formik.setFieldValue("password", password)}
       />
-      <Button title="Entrar" onPress={() => console.log("entrando")} />
+      <Button title="Entrar" onPress={formik.handleSubmit} />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
 }
+
+const initialValues = () => {
+  return {
+    username: "",
+    password: "",
+  };
+};
+
+const validationSchema = () => {
+  return {
+    username: Yup.string().required("El usuario es obligatorio"),
+    password: Yup.string().required("La contraseña es obligatoria"),
+  };
+};
 
 const styles = StyleSheet.create({
   title: {
@@ -42,5 +73,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+  },
+  error: {
+    textAlign: "center",
+    color: "#f00",
+    marginTop: 20,
   },
 });
