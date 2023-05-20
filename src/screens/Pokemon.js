7e5c7521a87be868010ff99bb2808bbcbefc5a08
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-20 19:10:34 -0300

    feat: add statistics layout

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -3,6 +3,7 @@ import { getPokemonDetail } from "../api/pokemon";
 import { useEffect, useState } from "react";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
 
 export default function Pokemon(props) {
   const {
@@ -33,6 +34,7 @@ export default function Pokemon(props) {
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
