78534caddc9f294785d76076ef841a56d8a62d50
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-21 18:22:12 -0300

    feat: favorite pokemon list

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,17 +1,52 @@
-import React from "react";
-import { Text, SafeAreaView, Button } from "react-native";
+import React, { useState, useCallback } from "react";
+import { Text, SafeAreaView } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 import { getPokemonFavoritesApi } from "../api/favorite";
+import { getPokemonDetail } from "../api/pokemon";
+import useAuth from "../hooks/useAuth";
+import PokemonList from "../components/PokemonList";
 
 export default function Favorite() {
-  const checkFavorites = async () => {
-    const response = await getPokemonFavoritesApi();
-    console.log(response);
-  };
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          try {
+            const response = await getPokemonFavoritesApi();
+            const pokemonArray = [];
+
+            for await (const id of response) {
+              const pokemonDetails = await getPokemonDetail(id);
+
+              pokemonArray.push({
+                id: pokemonDetails.id,
+                name: pokemonDetails.name,
+                type: pokemonDetails.types[0].type.name,
+                order: pokemonDetails.order,
+                image:
+                  pokemonDetails.sprites.other["official-artwork"]
+                    .front_default,
+              });
+            }
+            setPokemons(pokemonArray);
+          } catch (error) {
+            console.error(error);
+          }
+        })();
+      }
+    }, [auth])
+  );
 
   return (
     <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title="Ver favoritos" onPress={checkFavorites} />
+      {auth ? (
+        <PokemonList pokemons={pokemons} />
+      ) : (
+        <Text>Usuario no logueado</Text>
+      )}
     </SafeAreaView>
   );
 }
